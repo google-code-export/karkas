ccee2e116ba8b6782cbe1b01afba53c6e1ceee17
aozgur <aozgur@ac15386d-a63e-0410-a15c-c17449491352>
2007-09-26 15:07:36 +0000

    test projesi, mbunit eklendi. ConnectionHelperTest.cs eklendi.

--- codeGeneration/Simetri.MyGeneration/Simetri.MyGenerationTest/TurkishHelperTest.cs
+++ codeGeneration/Simetri.MyGeneration/Simetri.MyGenerationTest/TurkishHelperTest.cs
@@ -8,6 +8,7 @@ using Simetri.MyGenerationHelper;
 namespace Simetri.MyGenerationTest
 {
     [TestFixture]
+    [Author("Atilla Özgür", "aozgur@simetri.com")]
     public class TurkishHelperTest
     {
         string hataMesaji = "Turce yardýmcý duzgun calýþmýyor";
@@ -17,10 +18,6 @@ namespace Simetri.MyGenerationTest
             TurkishHelper tHelper = new TurkishHelper();
             string ingilizce1 = "Adi";
             string turkce1 = "Adi";
-            //Console.WriteLine(tHelper.TurkceyeCevir("Adi"));
-            //Console.WriteLine(tHelper.TurkceyeCevir("Soyadi"));
-
-            //Console.WriteLine(tHelper.ReplaceTurkishChars());
             Assert.IsTrue(tHelper.ReplaceTurkishChars(ingilizce1) == turkce1,hataMesaji );
             Assert.IsTrue(tHelper.ReplaceTurkishChars("Þanlýurfa") == "Sanliurfa", hataMesaji);
             Assert.IsFalse(tHelper.ReplaceTurkishChars("Þanlýurfa") == "Þanlýurfa", hataMesaji);
