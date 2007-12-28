a3a1975bdac43dbb10c6f2efab32f1666c580d8a
ati.ozgur <ati.ozgur@ac15386d-a63e-0410-a15c-c17449491352>
2007-12-28 15:20:45 +0000

    Basit hali ile tablodan aspxForm uretiyor.

--- codeGeneration/Simetri.MyGeneration/Simetri.MyGenerationConsoleTest/Program.cs
+++ codeGeneration/Simetri.MyGeneration/Simetri.MyGenerationConsoleTest/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Simetri.MyGenerationHelper;
 using Simetri.MyGenerationHelper.Generators;
+using System.Configuration;
 
 namespace Simetri.MyGenerationConsoleTest
 {
@@ -10,15 +11,8 @@ namespace Simetri.MyGenerationConsoleTest
     {
         public static void Main(string[] args)
         {
-            //EnumHelper eh = new EnumHelper();
-            //string enumSonuc = eh.GetEnumDescription("ITO", "TT_ORTAK", "SEHIR", connectionString);
-            //Console.WriteLine(enumSonuc);
-            DalGenerator dalGenerator = new DalGenerator();
-            Zeus.ZeusOutput output = new Zeus.ZeusOutput();
 
-            MyMeta.Sql.SqlTable table = new MyMeta.Sql.SqlTable();
 
-            dalGenerator.Render(output, table);
 
         }
     }
