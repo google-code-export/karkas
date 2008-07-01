6512c7779a4052bb2d8bc98cdea221e1d76f0357
ati.ozgur <ati.ozgur@ac15386d-a63e-0410-a15c-c17449491352>
2008-07-01 19:27:56 +0000

    Son Yapılanlar

--- codeGeneration/Simetri.MyGeneration/Simetri.MyGenerationHelper/Generators/AspxCsGenerator.cs
+++ codeGeneration/Simetri.MyGeneration/Simetri.MyGenerationHelper/Generators/AspxCsGenerator.cs
@@ -15,16 +15,71 @@ namespace Simetri.MyGenerationHelper.Generators
         public void Render(IZeusOutput output, ITable table)
         {
             IDatabase database = table.Database;
-            string baseNamespace = parser.ProjeNamespaceIsminiAl(database);
-            string baseNamespaceWeb = baseNamespace + ".WebApp";
-            string tableName = SimetriUtils.SetPascalCase(table.Name);
-            string formName = tableName + "Form";
 
+            string baseNameSpace = SimetriUtils.NamespaceIniAlSchemaIle(database, table.Schema);
+            string baseNamespaceWeb = baseNameSpace + ".WebApp";
+
+            string className = SimetriUtils.SetPascalCase(table.Name);
+            string schemaName = SimetriUtils.SetPascalCase(table.Schema);
+            string classNameSpace = baseNamespaceWeb + "." + schemaName;
+            string formName = className + "Form";
+
+
+            string basePage = "KarkasBasePage";
+
+            renderUsing(output, table);
+
+            output.autoTabLn("namespace " + baseNamespaceWeb);
+            output.autoTabLn("{");
+            output.incTab();
+
+
+            output.autoTabLn(string.Format("public partial class {0} : {1}", formName, basePage));
+            output.autoTabLn("{");
+
+
+            output.incTab();
+            renderPageLoad(output);
+
+            // class tab
+            output.decTab();
+            output.autoTabLn("}");
+
+            // namespace tab
+            output.decTab();
+            output.autoTabLn("}");
 
             string savePath = Path.Combine(SimetriUtils.ProjeDizininiAl(database), "WebApp\\" + SimetriUtils.SetPascalCase(table.Schema) + "\\" + formName + ".aspx.cs");
             output.save(savePath, true);
             output.clear();
 
         }
+
+        private static void renderPageLoad(IZeusOutput output)
+        {
+            output.autoTabLn("protected void Page_Load(object sender, EventArgs e)");
+            output.autoTabLn("{");
+            output.incTab();
+            // Page Load Code Here
+            output.decTab();
+            output.autoTabLn("}");
+        }
+
+        public void renderUsing(IZeusOutput pOutput, ITable pTable)
+        {
+            string strUsings = @"
+using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+";
+            pOutput.writeln(strUsings);
+        }
+
     }
 }
