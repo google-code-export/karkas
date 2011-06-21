1ea0948dc113036745ea5744e290203ddb403ffa
ati.ozgur@gmail.com <ati.ozgur@gmail.com@ac15386d-a63e-0410-a15c-c17449491352>
2011-06-21 18:41:42 +0000

    Code Generation için ekran üstünde düzenlemeler

--- branches/codeGenerationYeni/Karkas.CodeGeneration/Karkas.CodeGeneration.WinApp/MainForm.Designer.cs
+++ branches/codeGenerationYeni/Karkas.CodeGeneration/Karkas.CodeGeneration.WinApp/MainForm.Designer.cs
@@ -32,6 +32,12 @@
             this.textBoxConnectionString = new System.Windows.Forms.TextBox();
             this.buttonTestConnectionString = new System.Windows.Forms.Button();
             this.labelConnectionStatus = new System.Windows.Forms.Label();
+            this.panelListe = new System.Windows.Forms.Panel();
+            this.listBoxTableListesi = new System.Windows.Forms.ListBox();
+            this.labelTabloListesi = new System.Windows.Forms.Label();
+            this.labelSchemaList = new System.Windows.Forms.Label();
+            this.comboBoxSchemaList = new System.Windows.Forms.ComboBox();
+            this.panelListe.SuspendLayout();
             this.SuspendLayout();
             // 
             // labelConnectionString
@@ -70,17 +76,68 @@
             this.labelConnectionStatus.TabIndex = 4;
             this.labelConnectionStatus.Text = "Bağlantı Denenmedi";
             // 
+            // panelListe
+            // 
+            this.panelListe.Controls.Add(this.listBoxTableListesi);
+            this.panelListe.Controls.Add(this.labelTabloListesi);
+            this.panelListe.Controls.Add(this.labelSchemaList);
+            this.panelListe.Controls.Add(this.comboBoxSchemaList);
+            this.panelListe.Location = new System.Drawing.Point(24, 113);
+            this.panelListe.Name = "panelListe";
+            this.panelListe.Size = new System.Drawing.Size(924, 496);
+            this.panelListe.TabIndex = 5;
+            // 
+            // listBoxTableListesi
+            // 
+            this.listBoxTableListesi.DisplayMember = "FULL_TABLE_NAME";
+            this.listBoxTableListesi.FormattingEnabled = true;
+            this.listBoxTableListesi.ItemHeight = 16;
+            this.listBoxTableListesi.Location = new System.Drawing.Point(193, 68);
+            this.listBoxTableListesi.Name = "listBoxTableListesi";
+            this.listBoxTableListesi.Size = new System.Drawing.Size(271, 308);
+            this.listBoxTableListesi.TabIndex = 3;
+            // 
+            // labelTabloListesi
+            // 
+            this.labelTabloListesi.AutoSize = true;
+            this.labelTabloListesi.Location = new System.Drawing.Point(20, 65);
+            this.labelTabloListesi.Name = "labelTabloListesi";
+            this.labelTabloListesi.Size = new System.Drawing.Size(88, 17);
+            this.labelTabloListesi.TabIndex = 2;
+            this.labelTabloListesi.Text = "Tablo Listesi";
+            // 
+            // labelSchemaList
+            // 
+            this.labelSchemaList.AutoSize = true;
+            this.labelSchemaList.Location = new System.Drawing.Point(20, 16);
+            this.labelSchemaList.Name = "labelSchemaList";
+            this.labelSchemaList.Size = new System.Drawing.Size(103, 17);
+            this.labelSchemaList.TabIndex = 1;
+            this.labelSchemaList.Text = "Schema Listesi";
+            // 
+            // comboBoxSchemaList
+            // 
+            this.comboBoxSchemaList.DisplayMember = "TABLE_SCHEMA";
+            this.comboBoxSchemaList.FormattingEnabled = true;
+            this.comboBoxSchemaList.Location = new System.Drawing.Point(197, 18);
+            this.comboBoxSchemaList.Name = "comboBoxSchemaList";
+            this.comboBoxSchemaList.Size = new System.Drawing.Size(242, 24);
+            this.comboBoxSchemaList.TabIndex = 0;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(985, 656);
+            this.Controls.Add(this.panelListe);
             this.Controls.Add(this.labelConnectionStatus);
             this.Controls.Add(this.buttonTestConnectionString);
             this.Controls.Add(this.textBoxConnectionString);
             this.Controls.Add(this.labelConnectionString);
             this.Name = "MainForm";
             this.Text = "Form1";
+            this.panelListe.ResumeLayout(false);
+            this.panelListe.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -92,6 +149,11 @@
         private System.Windows.Forms.TextBox textBoxConnectionString;
         private System.Windows.Forms.Button buttonTestConnectionString;
         private System.Windows.Forms.Label labelConnectionStatus;
+        private System.Windows.Forms.Panel panelListe;
+        private System.Windows.Forms.Label labelSchemaList;
+        private System.Windows.Forms.ComboBox comboBoxSchemaList;
+        private System.Windows.Forms.Label labelTabloListesi;
+        private System.Windows.Forms.ListBox listBoxTableListesi;
     }
 }
 
--- branches/codeGenerationYeni/Karkas.CodeGeneration/Karkas.CodeGeneration.WinApp/MainForm.cs
+++ branches/codeGenerationYeni/Karkas.CodeGeneration/Karkas.CodeGeneration.WinApp/MainForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using Karkas.Core.DataUtil;
 
 namespace Karkas.CodeGeneration.WinApp
 {
@@ -16,15 +17,22 @@ namespace Karkas.CodeGeneration.WinApp
         {
             InitializeComponent();
         }
-
+        SqlConnection connection;
+        AdoTemplate template;
         private void buttonTestConnectionString_Click(object sender, EventArgs e)
         {
             try
             {
-                SqlConnection connection = new SqlConnection(textBoxConnectionString.Text);
+                if (connection != null && connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+                connection = new SqlConnection(textBoxConnectionString.Text);
                 connection.Open();
+                template = new AdoTemplate(connection);
                 labelConnectionStatus.Text = "Bağlantı Başarılı";
-                connection.Close();
+                BilgileriDoldur();
+
 
             }
             catch (Exception ex)
@@ -33,6 +41,49 @@ namespace Karkas.CodeGeneration.WinApp
                 labelConnectionStatus.Text = "!!!!Bağlantı BAŞARISIZ!!!!";
             }
 
+        }
+        private const string SQL_SCHEMA_LIST = @"
+SELECT '__TUM_SCHEMALAR__' AS TABLE_SCHEMA FROM INFORMATION_SCHEMA.TABLES
+UNION
+SELECT DISTINCT TABLE_SCHEMA FROM INFORMATION_SCHEMA.TABLES
+";
+
+
+        private const string SQL_TABLE_LIST = @"
+SELECT TABLE_SCHEMA + '.' + TABLE_NAME AS FULL_TABLE_NAME FROM INFORMATION_SCHEMA.TABLES
+WHERE
+( (@TABLE_SCHEMA IS NULL) OR (@TABLE_SCHEMA = '__TUM_SCHEMALAR__') OR ( TABLE_SCHEMA = @TABLE_SCHEMA))
+AND
+TABLE_TYPE = 'BASE TABLE'
+ORDER BY FULL_TABLE_NAME
+";
+
+
+        private void BilgileriDoldur( )
+        {
+            comboBoxSchemaListDoldur();
+            listBoxTableListDoldur();
+            this.comboBoxSchemaList.SelectedValueChanged += new System.EventHandler(this.comboBoxSchemaList_SelectedValueChanged);
+        }
+
+        private void listBoxTableListDoldur()
+        {
+            ParameterBuilder builder = new ParameterBuilder();
+            builder.parameterEkle("@TABLE_SCHEMA", SqlDbType.VarChar, comboBoxSchemaList.Text);
+            DataTable dtTableList = template.DataTableOlustur(SQL_TABLE_LIST, builder.GetParameterArray());
+            listBoxTableListesi.DataSource = dtTableList;
+        }
+
+        private void comboBoxSchemaListDoldur( )
+        {
+            DataTable dtSchemaList = template.DataTableOlustur(SQL_SCHEMA_LIST);
+            comboBoxSchemaList.DataSource = dtSchemaList;
+            comboBoxSchemaList.Text = "__TUM_SCHEMALAR__";
+        }
+
+        private void comboBoxSchemaList_SelectedValueChanged(object sender, EventArgs e)
+        {
+            listBoxTableListDoldur();
         }
     }
 }
