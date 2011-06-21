8d0059246824ffbdaf43e393c7564f5e10fddc46
ati.ozgur@gmail.com <ati.ozgur@gmail.com@ac15386d-a63e-0410-a15c-c17449491352>
2011-06-21 19:47:56 +0000

    MyGeneration Kullanan son halini tag'e aldım. Artık üzerinde geliştirme yapılmayacak

--- codeGeneration/copyDll.ps1
+++
@@ -1,3 +0,0 @@
-copy Karkas.MyGeneration\Karkas.MyGenerationHelper\bin\Debug\Karkas.MyGenerationHelper.dll "C:\Program Files\MyGeneration13"
-copy Karkas.MyGeneration\Karkas.MyGenerationHelper\bin\Debug\Karkas.MyGenerationHelper.pdb "C:\Program Files\MyGeneration13"
-"Copied Dlls"
