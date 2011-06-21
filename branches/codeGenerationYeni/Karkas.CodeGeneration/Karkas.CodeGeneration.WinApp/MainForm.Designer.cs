d0598eae96f026fecfe5d46e98692cc4be45deba
ati.ozgur@gmail.com <ati.ozgur@gmail.com@ac15386d-a63e-0410-a15c-c17449491352>
2011-06-21 17:47:17 +0000

    Kodun ilk hali

---
+++ branches/codeGenerationYeni/Karkas.CodeGeneration/Karkas.CodeGeneration.WinApp/MainForm.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Karkas.CodeGeneration.WinApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelConnectionString = new System.Windows.Forms.Label();
+            this.textBoxConnectionString = new System.Windows.Forms.TextBox();
+            this.buttonTestConnectionString = new System.Windows.Forms.Button();
+            this.labelConnectionStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelConnectionString
+            // 
+            this.labelConnectionString.AutoSize = true;
+            this.labelConnectionString.Location = new System.Drawing.Point(43, 35);
+            this.labelConnectionString.Name = "labelConnectionString";
+            this.labelConnectionString.Size = new System.Drawing.Size(120, 17);
+            this.labelConnectionString.TabIndex = 0;
+            this.labelConnectionString.Text = "Connection String";
+            // 
+            // textBoxConnectionString
+            // 
+            this.textBoxConnectionString.Location = new System.Drawing.Point(203, 35);
+            this.textBoxConnectionString.Name = "textBoxConnectionString";
+            this.textBoxConnectionString.Size = new System.Drawing.Size(624, 22);
+            this.textBoxConnectionString.TabIndex = 2;
+            this.textBoxConnectionString.Text = "Data Source=localhost;Initial Catalog=KARKAS_ORNEK;Integrated Security=True";
+            // 
+            // buttonTestConnectionString
+            // 
+            this.buttonTestConnectionString.Location = new System.Drawing.Point(846, 32);
+            this.buttonTestConnectionString.Name = "buttonTestConnectionString";
+            this.buttonTestConnectionString.Size = new System.Drawing.Size(103, 24);
+            this.buttonTestConnectionString.TabIndex = 3;
+            this.buttonTestConnectionString.Text = "test";
+            this.buttonTestConnectionString.UseVisualStyleBackColor = true;
+            this.buttonTestConnectionString.Click += new System.EventHandler(this.buttonTestConnectionString_Click);
+            // 
+            // labelConnectionStatus
+            // 
+            this.labelConnectionStatus.AutoSize = true;
+            this.labelConnectionStatus.Location = new System.Drawing.Point(52, 72);
+            this.labelConnectionStatus.Name = "labelConnectionStatus";
+            this.labelConnectionStatus.Size = new System.Drawing.Size(135, 17);
+            this.labelConnectionStatus.TabIndex = 4;
+            this.labelConnectionStatus.Text = "Bağlantı Denenmedi";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(985, 656);
+            this.Controls.Add(this.labelConnectionStatus);
+            this.Controls.Add(this.buttonTestConnectionString);
+            this.Controls.Add(this.textBoxConnectionString);
+            this.Controls.Add(this.labelConnectionString);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelConnectionString;
+        private System.Windows.Forms.TextBox textBoxConnectionString;
+        private System.Windows.Forms.Button buttonTestConnectionString;
+        private System.Windows.Forms.Label labelConnectionStatus;
+    }
+}
+
---
+++ branches/codeGenerationYeni/Karkas.CodeGeneration/Karkas.CodeGeneration.WinApp/MainForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Karkas.CodeGeneration.WinApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonTestConnectionString_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(textBoxConnectionString.Text);
+                connection.Open();
+                labelConnectionStatus.Text = "Bağlantı Başarılı";
+                connection.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                labelConnectionStatus.Text = "!!!!Bağlantı BAŞARISIZ!!!!";
+            }
+
+        }
+    }
+}
