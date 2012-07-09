b51b4ca1f9e0e589639fe29d00275d06f8e4479f
ati.ozgur <ati.ozgur@ac15386d-a63e-0410-a15c-c17449491352>
2012-07-09 14:49:45 +0000

    İlk hali

---
+++ trunk/codeGeneration/Karkas.CodeGeneration/Karkas.CodeGeneration.Oracle/OracleHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Karkas.CodeGenerationHelper;
+
+namespace Karkas.CodeGeneration.Oracle
+{
+    public class OracleHelper : IDatabaseHelper
+    {
+        public string getDatabaseName(Core.DataUtil.AdoTemplate template)
+        {
+            throw new NotImplementedException();
+        }
+
+        public System.Data.DataTable getTableListFromSchema(Core.DataUtil.AdoTemplate template, string schemaName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public System.Data.DataTable getSchemaList(Core.DataUtil.AdoTemplate template)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
