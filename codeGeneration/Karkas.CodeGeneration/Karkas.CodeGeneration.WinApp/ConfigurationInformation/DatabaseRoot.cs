301a7280158a64ad62ac23caed0314731412ade8
ati.ozgur <ati.ozgur@ac15386d-a63e-0410-a15c-c17449491352>
2012-07-09 08:58:17 +0000

    Object Database Volante üstünden Database Connection String değerleri alınıyor.

---
+++ codeGeneration/Karkas.CodeGeneration/Karkas.CodeGeneration.WinApp/ConfigurationInformation/DatabaseRoot.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Volante;
+
+namespace Karkas.CodeGeneration.WinApp.ConfigurationInformation
+{
+    public class DatabaseRoot : Persistent
+    {
+
+        public IIndex<string, DatabaseEntry> IndexName;
+        public IIndex<DateTime, DatabaseEntry> IndexLastWriteTime;
+        public IIndex<DateTime, DatabaseEntry> IndexLastAccessTime;
+        public IIndex<DateTime, DatabaseEntry> IndexCreationTime;
+
+        private static DatabaseRoot dbRootinstance;
+
+        public static DatabaseRoot DbRootInstance
+        {
+            get { return DatabaseRoot.dbRootinstance; }
+        }
+
+
+        private static IDatabase db = null;
+        public static void closeDatabase()
+        {
+            db.Close();
+        }
+
+        public static void openDatabase()
+        {
+            db = DatabaseFactory.CreateDatabase();
+            db.Open("connectionDatabase.dbs");
+            createDatabaseRoot();
+            db.Commit();
+
+        }
+
+        private static void createDatabaseRoot()
+        {
+
+            if (null != db.Root)
+            {
+                dbRootinstance = (DatabaseRoot)db.Root;
+            }
+            else
+            {
+                // only create root the first time
+                dbRootinstance = new DatabaseRoot();
+                DbRootInstance.IndexName = db.CreateIndex<string, DatabaseEntry>(IndexType.NonUnique);
+                DbRootInstance.IndexLastWriteTime = db.CreateIndex<DateTime, DatabaseEntry>(IndexType.NonUnique);
+                DbRootInstance.IndexLastAccessTime = db.CreateIndex<DateTime, DatabaseEntry>(IndexType.NonUnique);
+                DbRootInstance.IndexCreationTime = db.CreateIndex<DateTime, DatabaseEntry>(IndexType.NonUnique);
+
+                db.Root = DbRootInstance;
+                // changing the root marks database as modified but it's
+                // only modified in memory. Commit to persist changes to disk.
+                createExampleDatabaseEntry();
+            }
+        }
+
+        private static void createExampleDatabaseEntry()
+        {
+            DatabaseEntry de = getExampleDatabaseEntry();
+
+            addToIndexes(de);
+
+            db.Commit();
+        }
+
+        private static void addToIndexes(DatabaseEntry de)
+        {
+            DbRootInstance.IndexName.Put(de.ConnectionName, de);
+            DbRootInstance.IndexLastAccessTime.Put(de.LastAccessTimeUtc, de);
+            DbRootInstance.IndexLastWriteTime.Put(de.LastWriteTimeUtc, de);
+            DbRootInstance.IndexCreationTime.Put(de.CreationTimeUtc, de);
+        }
+
+        private static DatabaseEntry getExampleDatabaseEntry()
+        {
+            DatabaseEntry de = new DatabaseEntry();
+            de.CodeGenerationDirectory = "D:\\projects\\karkas\\Karkas.Ornek";
+            de.CodeGenerationNamespace = "Karkas.Ornek";
+            de.ConnectionDatabaseType = DatabaseType.SqlServer;
+            de.ConnectionName = "KARKAS_ORNEK";
+            de.ConnectionString = "Integrated Security = SSPI; Persist Security Info=False;Initial Catalog=KARKAS_ORNEK;Data Source=localhost";
+            de.CreationTimeUtc = DateTime.UtcNow;
+            de.LastWriteTimeUtc = DateTime.UtcNow;
+            de.LastAccessTimeUtc = DateTime.UtcNow;
+            return de;
+        }
+
+    }
+}
---
+++ codeGeneration/Karkas.CodeGeneration/Karkas.CodeGeneration.WinApp/ConfigurationInformation/DatabaseType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Karkas.CodeGeneration.WinApp.ConfigurationInformation
+{
+    public enum DatabaseType
+    {
+        SqlServer = 1,
+        Oracle = 2
+    }
+}
