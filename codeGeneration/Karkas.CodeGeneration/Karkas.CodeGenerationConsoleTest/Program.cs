1570adbffff0ee9538a475cac5620f37e41a3990
ati.ozgur <ati.ozgur@ac15386d-a63e-0410-a15c-c17449491352>
2012-07-10 14:41:15 +0000

    Oracle Code Generation asıl yapı 2

--- codeGeneration/Karkas.CodeGeneration/Karkas.CodeGenerationConsoleTest/Program.cs
+++ codeGeneration/Karkas.CodeGeneration/Karkas.CodeGenerationConsoleTest/Program.cs
@@ -10,16 +10,46 @@ using Karkas.CodeGenerationHelper.SmoHelpers;
 using Karkas.CodeGeneration.SqlServer;
 using Karkas.CodeGenerationHelper.Interfaces;
 using Karkas.CodeGeneration.SqlServer.Implementations;
+using Karkas.CodeGeneration.Oracle;
+using System.Reflection;
+using System.Runtime.Remoting;
+using System.Data.Common;
 
 namespace Karkas.MyGenerationConsoleTest
 {
     public class Program
     {
         public const string _SqlServerExampleConnectionString = "Data Source=localhost;Initial Catalog=KARKAS_ORNEK;Integrated Security=True";
-
+        public const string _OracleExampleConnectionString = "Data Source=ORACLEDEVDAYS;Persist Security Info=True;User ID=hr;Password=hr;Unicode=True";
+        
         public static void Main(string[] args)
         {
-            //SqlServerTest();
+            DbConnection connection = null;
+            AdoTemplate template = new AdoTemplate();
+                                Assembly oracleAssembly = Assembly.LoadWithPartialName("System.Data.OracleClient");
+                    Object objReflection = Activator.CreateInstance(oracleAssembly.FullName, "System.Data.OracleClient.OracleConnection");
+
+                    if (objReflection != null && objReflection is ObjectHandle)
+                    {
+                        ObjectHandle handle = (ObjectHandle)objReflection;
+
+                        Object objConnection = handle.Unwrap();
+                        connection = (DbConnection)objConnection;
+                        connection.ConnectionString = _OracleExampleConnectionString;
+                        connection.Open();
+                        connection.Close();
+                        ConnectionSingleton.Instance.ConnectionString = _OracleExampleConnectionString;
+                        ConnectionSingleton.Instance.ProviderName = "System.Data.OracleClient";
+                        template = new AdoTemplate();
+                        template.Connection = connection;
+                    }
+            IDatabaseHelper helper = new OracleHelper();
+
+
+            helper.CodeGenerateOneTable(template, _OracleExampleConnectionString, "COUNTRIES", "HR", "ORACLEDEVDAYS", "Karkas.OracleExample", "D:\\projects\\karkas\\Karkas.OracleExample");
+
+
+            helper.CodeGenerateAllTables(template, _OracleExampleConnectionString, "ORACLEDEVDAYS", "Karkas.OracleExample", "D:\\projects\\karkas\\Karkas.OracleExample", true, true);
         }
 
         private static void SqlServerTest()
