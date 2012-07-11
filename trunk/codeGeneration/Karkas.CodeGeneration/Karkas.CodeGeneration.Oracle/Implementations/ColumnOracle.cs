777d65d08356ccacf5ec9086988470d79be3a5f7
ati.ozgur <ati.ozgur@ac15386d-a63e-0410-a15c-c17449491352>
2012-07-11 09:13:11 +0000

    column is primary key added.

--- trunk/codeGeneration/Karkas.CodeGeneration/Karkas.CodeGeneration.Oracle/Implementations/ColumnOracle.cs
+++ trunk/codeGeneration/Karkas.CodeGeneration/Karkas.CodeGeneration.Oracle/Implementations/ColumnOracle.cs
@@ -3,20 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Karkas.CodeGenerationHelper.Interfaces;
+using Karkas.Core.DataUtil;
+using System.Data;
 
 namespace Karkas.CodeGeneration.Oracle.Implementations
 {
     class ColumnOracle : IColumn
     {
-        public ColumnOracle(TableOracle pTable,string pName)
+        public ColumnOracle(AdoTemplate pTemplate, TableOracle pTable, string pName)
         {
+
+            template = pTemplate;
             table = pTable;
             name = pName;
 
         }
 
-        TableOracle table;
-        string name;
+        private AdoTemplate template;
+
+        private TableOracle table;
+        private string name;
 
 
         public bool IsAutoKey
@@ -29,9 +35,48 @@ namespace Karkas.CodeGeneration.Oracle.Implementations
             get { return name; }
         }
 
+        private bool? isInPrimaryKey;
+
+        private const string SQL_PRIMARY_KEY = @" SELECT
+  COUNT(*)
+    FROM all_constraints cons
+    INNER JOIN 
+    all_cons_columns cols
+ON
+   cons.constraint_name = cols.constraint_name
+
+   WHERE     cols.table_name = :tableName
+         AND COLS.OWNER = :schemaName
+         AND COLS.COLUMN_NAME =  :columnName
+         AND cons.constraint_type = 'P'
+         AND cons.owner = cols.owner";
+
+
         public bool IsInPrimaryKey
         {
-            get { throw new NotImplementedException(); }
+            get
+            {
+                if (!isInPrimaryKey.HasValue)
+                {
+                    ParameterBuilder builder = new ParameterBuilder();
+                    builder.parameterEkle("tableName", DbType.String, Table.Name);
+                    builder.parameterEkle("schemaName", DbType.String, Table.Schema);
+                    builder.parameterEkle("columnName", DbType.String, Name);
+                    Object objSonuc = template.TekDegerGetir(SQL_PRIMARY_KEY, builder.GetParameterArray());
+                    Decimal sonuc = (Decimal)objSonuc;
+                    if (sonuc > 0)
+                    {
+                        isInPrimaryKey = true;
+                    }
+                    else
+                    {
+                        isInPrimaryKey = false;
+                    }
+
+                }
+                return isInPrimaryKey.Value;
+
+            }
         }
 
         public bool IsInForeignKey
