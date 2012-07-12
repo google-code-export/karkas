79084339fbb2e24ff5b4733fe46e5ece030a9a77
ati.ozgur <ati.ozgur@ac15386d-a63e-0410-a15c-c17449491352>
2012-07-12 14:04:34 +0000

    Abbravetions ilk hali

---
+++ trunk/codeGeneration/Karkas.CodeGeneration/Karkas.CodeGeneration.WinApp/ConfigurationInformation/DatabaseAbbreviations.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Karkas.CodeGeneration.WinApp.ConfigurationInformation
+{
+    public class DatabaseAbbreviations
+    {
+        public string Abbravetion { get; set; }
+        public string FullNameReplacement { get; set; }
+    }
+}
